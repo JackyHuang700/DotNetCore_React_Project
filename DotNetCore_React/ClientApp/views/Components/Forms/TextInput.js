40c2bd0e2ffb08b6bb9011431334aece32233fe3
Aaron <Aaron.fan@e-record.com.tw>
2017-08-12 01:01:31 +0800

    語法糖好好吃

--- DotNetCore_React/ClientApp/views/Components/Forms/TextInput.js
+++ DotNetCore_React/ClientApp/views/Components/Forms/TextInput.js
@@ -9,85 +9,35 @@ export default class TextInput extends Component {
     }
 
     render() {
-            let baseField = <
-                Field
-            type = { this.props.type }
-            id = { this.props.name }
-            name = { this.props.name }
-            className = { this.props.className }
-            onChange = { this.props.onChange }
-            defaultValue = { this.props.defaultValue }
-            placeholder = { this.props.placeholder }
-            />
+        const myProps = Object.assign({},this.props);
+        delete myProps.labelName;
 
-            if (this.props.required) {
-                baseField = React.cloneElement(baseField, {
-                    required: 'required',
-                    validMessage: this.props.validMessage
-                });
-            }
+        let baseField = <Field {...myProps} />
+        baseField = React.cloneElement(baseField,{
+            type:this.props.type,
+            id:this.props.name,
+            name:this.props.name,
+            className:this.props.className,
+            onChange:this.props.onChange,
+            defaultValue:this.props.defaultValue,
+            placeholder:this.props.placeholder
+        });
 
-            if (this.props.pattern) {
-                baseField = React.cloneElement(baseField, {
-                    pattern: this.props.pattern
-                });
-            }
+        if (!this.props.display) {
+            return (<div></div>)
+        }
 
-            if (this.props.confirm) {
-                baseField = React.cloneElement(baseField, {
-                    confirm: this.props.confirm
-                });
-            }
+        return ( <FormGroup>
+                <label style = {{ color: this.props.required && 'red' }} > { this.props.labelName } { this.props.required && '*' } </label> 
+                { baseField } 
+        </FormGroup>)
+    }
+}
 
-            if (this.props.minLength) {
-                baseField = React.cloneElement(baseField, {
-                    minLength: this.props.minLength
-                });
-            }
-
-            if (this.props.maxLength) {
-                baseField = React.cloneElement(baseField, {
-                    maxLength: this.props.maxLength
-                });
-            }
-
-            if (this.props.min) {
-                baseField = React.cloneElement(baseField, {
-                    min: this.props.min
-                });
-            }
-
-            if (this.props.max) {
-                baseField = React.cloneElement(baseField, {
-                    max: this.props.max
-                });
-            }
-
-
-            if (this.props.readOnly) {
-                baseField = React.cloneElement(baseField, {
-                    readOnly: this.props.readOnly
-                });
-            }
-
-            if (!this.props.display) {
-                return ( < div > < /div>)
-                }
-
-
-
-                return ( < FormGroup >
-                    <
-                    label style = {
-                        { color: this.props.required && 'red' } } > { this.props.labelName } { this.props.required && '*' } < /label> { baseField } <
-                    /FormGroup>)        
-                }
-            }
-
-            TextInput.defaultProps = {
-                display: true,
-                type: 'text',
-                className: '',
-                validMessage: null,
-                readOnly: false,
-            }
+TextInput.defaultProps = {
+    display: true,
+    type: 'text',
+    className: '',
+    validMessage: null,
+    readOnly: false,
+}
