1bf6ba76fc2a958321fcb32b7d04e5f5e3210f92
Jacky <jacky8595125@gmail.com>
2017-08-12 17:12:44 +0800

    add controller  News, News_Lan, Sys_Language

---
+++ DotNetCore_React/Controllers/Backend/NewsController.cs
@@ -0,0 +1,67 @@
+﻿using DotNetCore_React.Application.NewsApp;
+using DotNetCore_React.Application.NewsApp.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore_React.Controllers
+{
+    /// <summary>
+    /// 角色
+    /// </summary>
+    [Route("api/[controller]")]
+    public class NewsController : AuthorizedController
+    {
+        private readonly INewsAppService _service;
+
+        public NewsController(INewsAppService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("[action]")]
+
+        public ActionResult Get_News(string id)
+        {
+            var myJson = _service.GetSingle(id);
+            return Json(myJson);
+        }
+
+
+        [HttpGet("[action]")]
+
+        public ActionResult News_View()
+        {
+            var myJson = _service.GetAll();
+            return Json(myJson);
+        }
+
+
+        [HttpPost("[action]")]
+
+        public ActionResult Create([FromBody] NewsDto news)
+        {
+            var myJson = _service.Create(news);
+            return Json(myJson);
+        }
+
+        [HttpPost("[action]")]
+
+        public ActionResult Edit([FromBody] NewsDto news)
+        {
+            var myJson = _service.Update(news);
+            return Json(myJson);
+        }
+
+
+        [HttpPost("[action]/{id}")]
+
+        public ActionResult Delete(string id)
+        {
+            var myJson = _service.Delete(id);
+            return Json(myJson);
+        }
+    }
+}
