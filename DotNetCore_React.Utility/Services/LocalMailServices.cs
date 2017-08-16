47de1b014bc71aaed5679f51f9f874d63c178155
Jacky <jacky8595125@gmail.com>
2017-08-16 15:52:58 +0800

    DotNetCore_React.Utility 新增MailKit 套件
    新增Interface 用偷的

---
+++ DotNetCore_React.Utility/Services/LocalMailServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using DotNetCore_React.Utility.Interface;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace DotNetCore_React.Utility.Services
+{
+    public class LocalMailServices : IMailServices
+    {
+
+    }
+}
